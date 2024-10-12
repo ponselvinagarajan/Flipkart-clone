81f84eb0c0659d6c35830576121855f6d00d9064
ponselvinagarajan <ponselvinagarajan@gmail.com>
2024-10-12 10:23:48 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,42 @@
-
 import './App.css';
 import Home from '../src/pages/Home/Home';
 import Product from '../src/pages/Product/Product';
 import ProductDetails from '../src/pages/ProductDetails/ProductDetails';
 import Cart from '../src/pages/Cart/Cart';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from '../src/Components/Navbar/Navbar';
 import supabase from "./supabase";
 import { useDispatch } from "react-redux";
 import { setUser } from "./Slice/UserSlice";
 import { useEffect } from 'react';
+
+const getUser = async (dispatch) => {
+  const { data, error } = await supabase.auth.getSession();
+  if (error) {
+    console.error('Error fetching user session:', error);
+    return; // Handle error as needed
+  }
+  dispatch(setUser(data.session.user));
+};
+
 function App() {
   const dispatch = useDispatch();
 
-  const getUser = async () => {
-    const { data, error } = await supabase.auth.getSession();
-    dispatch(setUser(data.session.user));
-  };
-
   useEffect(() => {
-    getUser();
-  }, []);
+    getUser(dispatch);
+  }, [dispatch]); // Include dispatch
+
   return (
     <BrowserRouter>
-      <Navbar/>
-        <Routes>
-           <Route path="/" element={< Home/>}/>
-           <Route path="/Product" element={< Product/>}/>
-           <Route path="/ProductDetails/:id" element={< ProductDetails/>}/>
-           <Route path="/Cart" element={< Cart/>}/>
-        </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Product" element={<Product />} />
+        <Route path="/ProductDetails/:id" element={<ProductDetails />} />
+        <Route path="/Cart" element={<Cart />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
